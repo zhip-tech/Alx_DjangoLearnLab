2c1f6b850a723799220c3153e8333d787baf6043
Ogochukwu Ike <ogochike508@gmail.com>
2026-01-24 12:36:14 +0100

    ggk

--- LibraryProject/relationship_app/LibraryProject/settings.py
+++ LibraryProject/relationship_app/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'relationship_app',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Optional: for media files (profile photos)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
--- LibraryProject/relationship_app/admin.py
+++ LibraryProject/relationship_app/admin.py
@@ -1,3 +1,25 @@
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Information", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    list_display = ("username", "email", "is_staff", "date_of_birth")
+
 
 # Register your models here.
