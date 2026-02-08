9edfc5b2b6b92094b972ffa70e622ce6ad46cfca
Ogochukwu Ike <ogochike508@gmail.com>
2026-02-08 20:41:25 +0100

    fifth

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,11 +1,12 @@
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-
 
 
 
